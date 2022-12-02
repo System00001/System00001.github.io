abffa9ad358ece8166c1f52e206cb379a9547140
System00001 <francisco.wordpress.system01@gmail.com>
2022-12-01 22:54:59 -0500

    Puente Hidráulico

---
+++ template/Footer.php
@@ -0,0 +1,13 @@
+  <!-- FOOTER -->
+  <footer class="container">
+    <p class="float-end"><a href="#">Volver arriba</a></p>
+    <p>&copy; 2021–2022 CBTIS-272, Furanshisuko.</p>
+  </footer>
+</main>
+
+
+    <script src="./js/bootstrap.bundle.min.js"></script>
+
+      
+  </body>
+</html>
